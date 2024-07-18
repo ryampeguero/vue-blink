8fdeb3c8571f9294960faf4d2b6aee542881bdfd
aniellonigrocogliano <aniello.nigro.cogliano@gmail.com>
2024-07-18 10:06:14 +0200

    add: login page and session

--- src/router.js
+++ src/router.js
@@ -4,6 +4,7 @@ import HomePage from "./pages/HomePage.vue";
 import About from "./pages/HomePage.vue";
 import Contacts from "./pages/Contacts.vue";
 import Profile from "./pages/Profile.vue";
+import Login from "./pages/Login.vue";
 
 export const router = createRouter({
     history: createWebHistory(),
@@ -27,6 +28,26 @@ export const router = createRouter({
             path: "/profile",
             name: "profile",
             component: Profile,
+            meta: { requiresAuth: true }, 
+        },
+        {
+            path: "/login",
+            name: "login",
+            component: Login,
         },
     ],
 });
+router.beforeEach((to, from, next) => {
+    const requiresAuth = to.matched.some(record => record.meta.requiresAuth);
+    const isAuthenticated = !!localStorage.getItem('authToken');
+  
+    if (requiresAuth && !isAuthenticated) {
+        next('/login');
+      } else if (to.path === '/login' && isAuthenticated) {
+        next('/profile');
+      } else {
+        next();
+      }
+    });
+  
+  export default router;
