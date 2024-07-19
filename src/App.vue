e1676842c9f0ea0a96d12d95e915a16ce56b9245
ryampeguero <ryampeguero@gmail.com>
2024-07-19 10:07:43 +0200

    fix: some git conflicts

--- src/App.vue
+++ src/App.vue
@@ -18,18 +18,11 @@ export default {
 </script>
 
 <template>
-<<<<<<< HEAD
-
-    <AppHeader />
-    <router-view></router-view>
-    <AppFooter />
-=======
     <div class="prime">
         <AppHeader />
         <router-view></router-view>
             <AppFooter />
     </div>
->>>>>>> 8557d70d9bf50d47609c841e4942d632b93864ec
 </template>
 
 <style lang="scss">
