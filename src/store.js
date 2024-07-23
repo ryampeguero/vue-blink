6adff29426ecd4986f6103e84feed90b762b4a46
ryampeguero <ryampeguero@gmail.com>
2024-07-23 11:11:26 +0200

    feat: Added new variables in store.js, improved Advanced search

--- src/store.js
+++ src/store.js
@@ -1,9 +1,30 @@
 import { reactive } from "vue";
 export const store = reactive({
-    token:"",
-    flatArray: [
+    token: "",
+    flatArray: [],
+    services: [
+        { id: 1, name: "Wi-Fi", icon: "fa-wifi" },
+
+        { id: 2, name: "Pulizia settimanale", icon: "fa-broom" },
+
+        { id: 3, name: "Aria condizionata", icon: "fa-snowflake" },
+
+        { id: 4, name: "Lavanderia", icon: "fa-tshirt" },
+
+        { id: 5, name: "Piscina", icon: "fa-swimmer" },
+
+        { id: 6, name: "Palestra", icon: "fa-dumbbell" },
+
+        { id: 7, name: "Parcheggio", icon: "fa-parking" },
+
+        { id: 8, name: "TV via cavo", icon: "fa-tv" },
+
+        { id: 9, name: "Animali ammessi", icon: "fa-paw" },
+
+        { id: 10, name: "Servizio di portineria", icon: "fa-concierge-bell" }
 
     ],
+    checkedIds: [],
     latitude: "",
     longitude: "",
 })
